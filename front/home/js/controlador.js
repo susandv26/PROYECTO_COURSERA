caaf69b6005d908af743344753b0b0b657c255cc
susandv26 <valladares.susan@yahoo.com>
2024-08-04 22:12:39 -0600

    arreglo en el navegador, poner cards en el body de inicio

--- front/home/js/controlador.js
+++ front/home/js/controlador.js
@@ -8,3 +8,29 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownItems = document.querySelectorAll('.dropdown-item');
+    const selectedLanguage = document.getElementById('selectedLanguage');
+
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            const selectedText = this.getAttribute('data-lang');
+            selectedLanguage.textContent = selectedText;
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownItems = document.querySelectorAll('.dropdown-item');
+    const selectedPerfil = document.getElementById('selectedPerfil');
+
+    dropdownItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            const selectedText = this.getAttribute('data-lang');
+            selectedPerfil.textContent = selectedText;
+        });
+    });
+});
